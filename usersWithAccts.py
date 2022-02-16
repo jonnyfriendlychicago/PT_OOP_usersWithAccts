a008e75577d35b1d17a9131ca4ed37f21b15481b
Jon Friend <62109917+jonnyfriendlychicago@users.noreply.github.com>
2022-02-16 16:34:49 -0600

    ready to review

--- usersWithAccts.py
+++ usersWithAccts.py
@@ -1,28 +1,64 @@
-class User:
-    bank_name = "First National Dojo"
-    all_accounts = []
 
-    def __init__(self, name, email_address):
-    	# we assign them accordingly
+class User:
+    # bank_name = "First National Dojo"
+    # all_accounts = []
+
+    def __init__(self, cust_num, name, email_address):
+        self.cust_num = cust_num
         self.name = name
         self.email = email_address
-    	# the account balance is set to $0
-        self.account_balance = 0
+        print(f"Customer account created.  Customer Num: {self.cust_num} | Customer Name: {self.name} | Email: {self.email}")
+        # self.account_balance = 0
+        # above replaced with below
+        self.account = BankAccount(acct_num=123, int_rate=0.05, balance=0)
+        # self.account = BankAccount(int_rate=0.02, balance=0) 
+
+    def transStart_Affirm (self):
+        print(f"+++ Transaction Begin +++ \nCustomer Name: {self.name} | Customer Num: {self.cust_num}")
+    
+    def transEnd_Affirm (self):
+        print("--- Transaction End ---")
 
     def make_deposit(self, amount):	# takes an argument that is the amount of the deposit
-        self.account_balance += amount	# the specific user's account increases by the amount of the value received
+        # self.account_balance += amount	# the specific user's account increases by the amount of the value received
+        # above replaced with below... work in progress
+        # self.account = BankAccount(acct_num = 123, int_rate=0.05, balance=0)
+        self.transStart_Affirm()
+        self.account.deposit(amount)
+        self.transEnd_Affirm()
         return self
 
-    def make_withdrawal(self, amount):
-    # have this method decrease the user's balance by the amount specified
-        self.account_balance -= amount
+    def make_withdrawal(self, amount):	# takes an argument that is the amount of the deposit
+        # self.account_balance += amount	# the specific user's account increases by the amount of the value received
+        # above replaced with below... work in progress
+        # self.account = BankAccount(acct_num = 123, int_rate=0.05, balance=0)
+        self.transStart_Affirm()
+        self.account.withdraw(amount)
+        self.transEnd_Affirm()
         return self
+
+    # def make_withdrawal(self, amount):
+    # # have this method decrease the user's balance by the amount specified
+    #     self.account_balance -= amount
+    #     return self
 
     def display_user_balance(self): 
-        self.balance_display = print(f"User: {self.name}, Balance: ${self.account_balance}")
+        # self.balance_display = print(f"User: {self.name}, Balance: ${self.account_balance}")
+        # above replaced by below
+        self.transStart_Affirm()
+        self.account.display_account_info()
+        self.transEnd_Affirm()
         return self
-    #have this method print the user's name and account balance to the terminal, e.g. "User: Guido van Rossum, Balance: $150
-
+    
+    def yield_interest(self): 
+        # self.balance_display = print(f"User: {self.name}, Balance: ${self.account_balance}")
+        # above replaced by below
+        self.transStart_Affirm()
+        self.account.yield_interest()
+        self.transEnd_Affirm()
+        return self
+    
+"""
     def transfer_money(self, other_user, amount): 
         self.account_balance -= amount
         other_user.account_balance += amount
@@ -43,92 +79,107 @@ class User:
             sum += account.balance
         return sum
 
-
-a1 = User("Lucky Day", "lucky@3amigos.com")
-a2 = User("Dusty Bottoms", "dusty@3amigos.com")
-a3 = User("Ned Nederlander", "ned@3amigos.com")
-
-print(a1.name)
-print(a2.name)
-print(a3.name)
-
-# a1.make_deposit(33)
-# a1.make_deposit(33)
-# a1.make_deposit(33)
-# a1.make_withdrawal(20)
-# a1.display_user_balance()
-
-a1.make_deposit(33).make_deposit(33).make_deposit(33).make_withdrawal(20).display_user_balance()
-
-# a2.make_deposit(50)
-# a2.make_deposit(50)
-# a2.make_withdrawal(25)
-# a2.make_withdrawal(25)
-# a2.display_user_balance()
-
-a2.make_deposit(50).make_deposit(50).make_withdrawal(25).make_withdrawal(25).display_user_balance()
-
-# a3.make_deposit(79)
-# a3.make_withdrawal(40)
-# a3.make_withdrawal(20)
-# a3.make_withdrawal(20)
-# a3.display_user_balance() 
-
-a3.make_deposit(79).make_withdrawal(40).make_withdrawal(20).make_withdrawal(20).display_user_balance()
-
-a1.transfer_money(a3, 19)
+"""
 
 class BankAccount:
-    # balance = 0
-    # int_rate = 0.01
+    # accounts = []
+    def __init__(self, acct_num, int_rate, balance):
+        self.acct_num = acct_num
+        self.int_rate = int_rate
+        self.balance = balance
+        # BankAccount.accounts.append(self)
+        #above newly added
+        print(f"Checking account created.  Acct Num: {self.acct_num}.  | Current balance is ${self.balance} with a current interest rate of {self.int_rate}.")
+        # return None
     
-    def __init__(self, int_rate, balance):
-        self.int_rate = 0.01
-        self.balance = 0
-        print(f"Accounted created.  Current balance is ${self.balance} with a current interest rate of {self.int_rate}.")
-        return self
+    def display_account_info (self):
+        # self.display_account_info = print(f"Balance: ${self.balance}")
+        #11pm here... just realized how funky above line is.  not sure why ever wrote it that way.  below is attempted fix
+        print(f"Balance: ${self.balance}")
+        # return self    
     
     def deposit (self, amount): 
         self.balance += amount
-        print(f"Deposit accepted: ${self.amount}.  New balance: ${self.balance}.")
-
-    def withdraw (self, amount):
-        if self.balance < self.amount:
-            self.balance -= 5
-            print("Insufficient funds: now we just charged you $5 fee... and you cant do shit about it.  Thank you for voting Republican!  We could not do this without their protection.")
-        else: 
-            self.balance -= amount
-            print(f"withdrawl of {self.amount} completed.  We will find another way to steal your money next time.")
+        # print(f"Deposit accepted: ${amount}.  New balance: ${self.balance}.")
+        print(f"Deposit accepted: ${amount}.")
+        # print(f"Balance: ${self.balance}")
+        self.display_account_info()
+        #***********************
+        # above line "self.display_account_info()" works here... but never works again in any of the other methods that try to use it.  WHY????
         return self
 
-    def display_account_info (self):
-        self.display_account_info = print(f"Balance: ${self.balance}")
+
+    def withdraw (self, amount):
+        if self.balance < amount:
+            self.old_balance = self.balance
+            self.balance -= 5
+            print(f"Insufficient funds.  You tried to withdraw ${amount} but your balance was only ${self.old_balance}.  For even attempting that, we just charged you a completely unjustifiable $5 fee... and you cant do jack about it.")
+            self.display_account_info()
+            # print(f"Balance: ${self.balance}")
+        else: 
+            self.balance -= amount
+            print(f"Withdrawl completed: ${amount} ")
+            self.display_account_info()
+            # print(f"Balance: ${self.balance}")
         return self
 
     def yield_interest(self):
         if self.balance > 0:
-            self.balance = self.balance * self.int_rate + self.balance
-            print(f"interest yield added to account.  New Account Balance: {self.balance}.")
+            self.int_added = self.balance * self.int_rate
+            self.balance =  self.int_added + self.balance
+            print(f"Interest yield added to account in the amount: ${self.int_added}.")
+            self.display_account_info()
+            # print(f"Balance: ${self.balance}")
         else: 
             print(f"No interest yield payment: account balance less than or equal to zero.")
-        
-
-
-
-
-    # below from lesson example 
-    """
-    def with_draw (self, amount):
-        if BankAccount.can_withdraw(self.balance, amount):
-            self.balance -= amount
-        else:
-            print("Insufficient Funds")
+            self.display_account_info()
+            # print(f"Balance: ${self.balance}")
         return self
+
+
+
+"""
+    @classmethod
+    def print_all_accounts(cls):
+        print("List of all account balances:::::::::::::::::::")
+        for x in cls.accounts:
+            x.display_account_info()
     
-    @staticmethod
-    def can_withdraw(balance, amount):
-        if (balance - amount) < 0:
-            return False
-        else: 
-            return True
-    """
+    @classmethod
+    def all_balances(cls):
+        sum = 0
+        for account in cls.all_accounts:
+            sum += account.balance
+        print(sum)
+
+"""
+
+# all of below is working good, comm out for now
+# acct1 = BankAccount(123, 0.02,100)
+# acct1.deposit(1).deposit(2).deposit(3).withdraw(4).yield_interest()
+
+# acct2 = BankAccount(456, 0.02,1000)
+# acct2.deposit(2).deposit(200).withdraw(20).withdraw(40).withdraw(60).withdraw(80).yield_interest()
+
+# BankAccount.print_all_accounts()
+
+a1 = User("ABC", "Lucky Day", "lucky@3amigos.com")
+# a2 = User("Dusty Bottoms", "dusty@3amigos.com")
+# a3 = User("Ned Nederlander", "ned@3amigos.com")
+
+# print(a1.name)
+# print(a2.name)
+# print(a3.name)
+
+a1.make_deposit(33).make_deposit(43).make_deposit(53).make_withdrawal(130).yield_interest().display_user_balance()
+
+# a2.make_deposit(50)
+# .make_deposit(50).make_withdrawal(25).make_withdrawal(25).display_user_balance()
+
+# a3.make_deposit(79)
+# .make_withdrawal(40).make_withdrawal(20).make_withdrawal(20).display_user_balance()
+
+# taking out below till ready
+# a1.transfer_money(a3, 19)
+
+
